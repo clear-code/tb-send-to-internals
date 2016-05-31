47b3cda878098a241ef5a48b8e3c7dd31665b79b
YUKI Hiroshi <shimoda@clear-code.com>
2016-05-31 16:45:42 +0900

    Fix syntax error

--- content/composeOverlay.js
+++ content/composeOverlay.js
@@ -75,7 +75,7 @@ var SendToInternalsHelper = {
         this.bundle.getString('alert.haveExternals.title'),
         this.bundle.getFormattedString('alert.haveExternals.text', [externals.map(function(aAddress) {
           return aAddress.address;
-        })].join('\n')])
+        }).join('\n')])
       );
       return false;
     }
